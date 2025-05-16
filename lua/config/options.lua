2d1703fef36294faa57f29711d7fe2bb531799a3
Aqil Mukhi <aqilmukhi123@gmail.com>
2025-05-15 19:16:45 -0700

    Updated Config + Added Plugins

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,3 +7,8 @@ vim.opt.tabstop = 4
 vim.opt.wrap = true
 
 vim.g.root_spec = {"cwd"}
+
+vim.g.autoformat = false
+
+vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+vim.opt.foldmethod = 'expr'
---
+++ lua/plugins/nvim-dap-ui.lua
@@ -0,0 +1,38 @@
+return
+{
+    {
+        "rcarriga/nvim-dap-ui",
+        opts =
+            {
+                layouts =
+                {
+                    {
+                        elements =
+                        {
+                            {
+                                id = "stacks",
+                                size = 0.5
+                            },
+                            {
+                                id = "scopes",
+                                size = 0.5
+                            }
+                        },
+                        position = "left",
+                        size = 40
+                    },
+                    {
+                        elements =
+                        {
+                            {
+                                id = "console"
+                            }
+                        },
+                        position = "bottom",
+                        size = 10
+                    }
+                }
+            }
+    },
+}
+
